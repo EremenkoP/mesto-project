2c84e1c4201be4d2e6bf197a2337bc54e801a3a9
EremenkoP <Eremenko.p.a@yandex.ru>
2021-11-10 18:46:30 +0400

    10.11.21
    Прописал подгрузку дефолтных карточек,  реализовал работу 2 модалок.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-// **Вынимаю все что нужно из страницы
-// вынимаю кнопки
+// **все что нужно из страницы
+// вынимаю глобальные кнопки
 const changesProfile = document.querySelector('.profile__button-changes-profile');
 const cardAdd = document.querySelector('.profile__button-add');
 // вынимаю попапы
@@ -11,17 +11,63 @@ const popupClose = document.querySelectorAll('.popup__button-close');
 const popupProfileClose = popupClose[0];
 const popupAddClose = popupClose[1];
 const popupImgClose = popupClose[2];
-// вынимаю значения
-const formElement = document.querySelector('.popup__form');
+// вынимаю значения форм для попапов
+const formElement = document.querySelectorAll('.popup__form');
+const formProfile = formElement[0];
+const formCard = formElement[1];
+// стандартный набор карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
 
 // **работа непосредственно со страницей и попапами
+// *загрузка начальных картинок
+const cardTemp = document.querySelector("#card__temp").content;
+
+initialCards.forEach(function(item){
+  let card = cardTemp.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__title').textContent = item.name;
+  card.querySelector('.card__image').setAttribute('src', item.link);
+  card.querySelector('.card__image').setAttribute('alt', item.name);
+  card.querySelector('.card__button').addEventListener('click', function (event){
+    event.target.classList.toggle('card__button_active')
+  });
+  card.querySelector('.card__button-trash').addEventListener('click', function (){
+    card.remove();
+  });
+  document.querySelector('.cards').prepend(card);
+});
+
 // *изменения профиля
 // открытие и закрытие попапа
 function popupProfileFunction(){
   popupProfile.classList.toggle('popup_opened');
-  let formName = formElement.querySelector('#name');
+  let formName = formProfile.querySelector('#name');
   formName.setAttribute('value', profileName.textContent);
-  let formProfesion = formElement.querySelector('#proffesion');
+  let formProfesion = formProfile.querySelector('#proffesion');
   formProfesion.setAttribute('value', profileProfesion.textContent);
 }
 changesProfile.addEventListener('click', popupProfileFunction);
@@ -29,21 +75,43 @@ popupProfileClose.addEventListener('click', popupProfileFunction);
 // Работа с именем и профессией
 let profileName = document.querySelector('.profile__name');
 let profileProfesion  = document.querySelector('.profile__profession');
-function formSubmitHandler (evt) {
+function profileChanges (evt) {
   evt.preventDefault();
-  let formName = formElement.querySelector('#name');
+  let formName = formProfile.querySelector('#name');
   profileName.textContent = formName.value;
-  let formProfesion = formElement.querySelector('#proffesion');
+  let formProfesion = formProfile.querySelector('#proffesion');
   profileProfesion.textContent = formProfesion.value;
   popupProfileFunction();
 }
-formElement.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', profileChanges);
 
-//*Попап добавление новой карточки
+//*Добавление новой карточки
+// открытие и закрытие попапа
 function popupAddFunction(){
   popupAdd.classList.toggle('popup_opened');
 }
 cardAdd.addEventListener('click', popupAddFunction);
 popupAddClose.addEventListener('click', popupAddFunction);
+// Добавление новой карточки
+function formAddCard (evt) {
+  evt.preventDefault();
+  let card = cardTemp.querySelector('.card').cloneNode(true);
+  let cardName = popupAdd.querySelector('#name-card');
+  card.querySelector('.card__title').textContent = cardName.value;
+  let cardImg = popupAdd.querySelector('#image-url');
+  card.querySelector('.card__image').setAttribute('src', cardImg.value);
+  card.querySelector('.card__image').setAttribute('alt', cardName.value);
+  card.querySelector('.card__button').addEventListener('click', function (event){
+    event.target.classList.toggle('card__button_active')
+  });
+  card.querySelector('.card__button-trash').addEventListener('click', function (){
+    card.remove();
+  });
+  document.querySelector('.cards').prepend(card);
+  popupAddFunction();
+}
+let popupLetCard = popupAdd.querySelector('.popup__button')
+formCard.addEventListener('submit', formAddCard);
 
-// открытие попапа с картинкой
+//*Попап с картинкой
+// открытие и закрытие попапа
